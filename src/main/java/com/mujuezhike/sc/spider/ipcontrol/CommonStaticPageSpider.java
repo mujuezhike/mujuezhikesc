326b3568c82cce91e6a577fb7ffb365cd1a5a6fb
boxu <boxu@10.0.0.32>
2017-11-21 11:44:51 +0800

    data

--- src/main/java/com/mujuezhike/sc/spider/ipcontrol/CommonStaticPageSpider.java
+++ src/main/java/com/mujuezhike/sc/spider/ipcontrol/CommonStaticPageSpider.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.security.MessageDigest;
 
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.CookieManager;
@@ -42,6 +43,7 @@ public class CommonStaticPageSpider {
 
 		String codenum = url.substring(url.lastIndexOf("/")+1);
 		codenum = codenum.substring(codenum.lastIndexOf("=")+1);
+		
 		HtmlPage page = webClient.getPage(url);
 		
 		try {
@@ -49,7 +51,7 @@ public class CommonStaticPageSpider {
 			System.out.println("qcc");
 			String s = page.asXml();
 			
-				getFileFromBytes(s,dir.getAbsolutePath()+ "\\"+codenum+".html");
+				getFileFromBytes(s,dir.getAbsolutePath()+ "/qcclist/"+string2MD5(codenum)+".html");
 				webClient.close();
 				
 				return true;
@@ -97,5 +99,33 @@ public class CommonStaticPageSpider {
 	     return file;  
 	     
 	 } 
+	 
+	//进行md5加密的工具类
+	    public static String string2MD5(String inStr){
+
+	        MessageDigest md5 = null;
+	        try{
+	            md5 = MessageDigest.getInstance("MD5");
+	        }catch (Exception e){
+	            System.out.println(e.toString());
+	            e.printStackTrace();
+	            return "";
+	        }
+	        char[] charArray = inStr.toCharArray();
+	        byte[] byteArray = new byte[charArray.length];
+
+	        for (int i = 0; i < charArray.length; i++)
+	            byteArray[i] = (byte) charArray[i];
+	        byte[] md5Bytes = md5.digest(byteArray);
+	        StringBuffer hexValue = new StringBuffer();
+	        for (int i = 0; i < md5Bytes.length; i++){
+	            int val = ((int) md5Bytes[i]) & 0xff;
+	            if (val < 16)
+	                hexValue.append("0");
+	            hexValue.append(Integer.toHexString(val));
+	        }
+	        return hexValue.toString();
+
+	    }
 
 }
--- src/main/java/com/mujuezhike/sc/spider/ipcontrol/QichachaListSpider.java
+++ src/main/java/com/mujuezhike/sc/spider/ipcontrol/QichachaListSpider.java
@@ -1,6 +1,11 @@
 package com.mujuezhike.sc.spider.ipcontrol;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,20 +13,58 @@ public class QichachaListSpider {
 
 	public static void main(String[] args) throws Exception{
 		
-		List<String> list = new ArrayList<String>();
-		list.add("abc");
-		list.add("文化");
-		list.add("南京");
-		list.add("北京");
-		list.add("化学");
-		list.add("医疗");
-		list.add("衣服");
+		List<String> list = getListFromFile(new File("/Users/boxu/procode/word/word1120.txt"));
 		
 		for(int i=0;i<list.size();i++) {
-			Thread.sleep(3000);
-			CommonStaticPageSpider.savePage("http://www.qichacha.com/search?key="+list.get(i), new File("C:\\zkbean\\qcclist"));
+			Thread.sleep(200);
+			CommonStaticPageSpider.savePage("http://www.qichacha.com/search?key="+list.get(i), new File("/Users/boxu/procode"));
 		}
 
+	}
+	
+	public static List<String> getListFromFile(File file){
+		
+		List<String> list = new ArrayList<String>();
+		BufferedReader reader = null;
+        try {
+        	String charset = getCharset(file);
+            reader = new BufferedReader(new InputStreamReader(new FileInputStream(file),charset));  
+            String tempString = "";
+            while ((tempString = reader.readLine()) != null) {
+            	
+            		list.add(tempString);
+            		
+            }
+        }catch(Exception e) {
+        	
+        		e.printStackTrace();
+        		
+        }
+		
+		return list;
+	}
+	
+	private static String getCharset(File file) throws IOException{  
+        
+        BufferedInputStream bin = new BufferedInputStream(new FileInputStream(file));    
+        int p = (bin.read() << 8) + bin.read();    
+          
+        String code = null;    
+          
+        switch (p) {    
+            case 0xefbb:    
+                code = "UTF-8";    
+                break;    
+            case 0xfffe:    
+                code = "Unicode";    
+                break;    
+            case 0xfeff:    
+                code = "UTF-16BE";    
+                break;    
+            default:    
+                code = "GBK";    
+        }    
+        return code;  
 	}
 
 }
